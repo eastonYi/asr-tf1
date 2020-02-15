432f75214a671f719044ec0a251f708bfa72dce8
eastonYi <yicheng1994@outlook.com>
2020-02-15 15:51:51 +0800

    local change

--- data_prepare.py
+++ data_prepare.py
@@ -16,11 +16,11 @@ def main():
     # save2tfrecord_multilabel(args.dataset_train, args.dirs.train.tfdata, size_file=10000000)
     #save2tfrecord_multilabel(args.dataset_untrain, args.dirs.untrain.tfdata, size_file=10000000)
 
-    # TFDataSaver(args.dataset_train, args.dirs.train.tfdata, args, size_file=10000, max_feat_len=3000).split_save()
-    # TFDataSaver(args.dataset_dev, args.dirs.dev.tfdata, args, size_file=10000, max_feat_len=3000).split_save()
+    TFDataSaver(args.dataset_train, args.dirs.train.tfdata, args, size_file=30000, max_feat_len=3000).split_save()
+    TFDataSaver(args.dataset_dev, args.dirs.dev.tfdata, args, size_file=10000, max_feat_len=3000).split_save()
     # print(args.data.dim_feature)
     # feat, label = readTFRecord(args.dirs.dev.tfdata, args, _shuffle=False, transform=True)
-    get_bucket(args.dirs.train.tfdata / 'feature_length.txt', args.num_batch_tokens, 180)
+#     get_bucket(args.dirs.train.tfdata / 'feature_length.txt', args.num_batch_tokens, 180)
 
 if __name__ == '__main__':
     import os
